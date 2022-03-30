45320faa9ef675cb760760484132b8a02fa03c8d
shenhai <326095953@qq.com>
2022-03-30 17:35:52 +0800

    路由设计

---
+++ src/components/router/index.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
+import React, { Suspense, lazy } from 'react';
+import ReactDOM from 'react-dom'
+const Login = lazy(() => import('../login/index'));
+const Home = lazy(() => import('../home/index'));
+const App = () => (
+  <Router>
+     <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route  path="/" element={<Home/>}/>
+        <Route  path="/login" element={<Login/>}/>
+      </Routes>
+    </Suspense>
+  </Router>
+)
+export default App
+
