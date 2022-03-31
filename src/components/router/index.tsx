7d9393af16bcbb8d38b6c03be274ee66539a3e3c
shenhai <326095953@qq.com>
2022-03-31 16:54:58 +0800

    流水线编译报错修改

--- src/components/router/index.tsx
+++ src/components/router/index.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
 import React, { Suspense, lazy } from 'react';
-import ReactDOM from 'react-dom'
+// import ReactDOM from 'react-dom'
 const Login = lazy(() => import('../login/index'));
 const Home = lazy(() => import('../home/index'));
 const App = () => (
